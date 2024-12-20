4d903a1704c06a1de31d2238c4cc5596b92eff99
Matthew <matthew.bate@sk-projects.net>
2024-12-19 19:29:40 -0500

    Code cleanup.

--- Panels/ClientLogin.cs
+++ Panels/ClientLogin.cs
@@ -8,23 +8,15 @@ namespace Proxmox_Desktop_Client.Panels
 {
     public partial class ClientLogin : Form
     {
-    
         public ClientLogin()
         {
-            // Init ClientForm
             InitializeComponent();
-            
-            // Store Panel for Global Reference
             Program._Panels.Add("ClientLogin", this);
-            
-            // Preset Client Login Form & Shrink Window
             panel_credentials.Visible = false;
             Width = 290;
-            
-            // Load Credentials from Storage and test if server accessible
             LoadCredentials();
         }
-        
+
         private void ClickOtp(object sender, EventArgs e)
         {
             textBox_otp.Text = string.Empty;
@@ -33,58 +25,45 @@ namespace Proxmox_Desktop_Client.Panels
 
         private void ClickCheckServer(object sender = null, EventArgs e = null)
         {
-            // Define & Pull Variables from Form
             string server = textBox_server.Text;
             string port = textBox_port.Text;
             bool validateSsl = checkBox_ssl.Checked;
-            
-            // Disable Fields & Shrink ClientLogin Window
+
             panel_credentials.Enabled = false;
             panel_server.Enabled = false;
             Width = 290;
-            
-            // Recenter Screen
             CenterToScreen();
-            
-            // Test Connection
-            Program._Api = new ApiClient(server, port, validateSsl);
-            
-            // Get Realms            
-            List<RealmData> realms = Program._Api.GetRealms();
-            
-            // Realms Valid
-            if (realms != null && realms.Count > 0)
+
+            try
             {
-                // Attach Data to Realms comboBox
-                comboBox_realm.DataSource = realms;
-                comboBox_realm.DisplayMember = "Comment";
-                comboBox_realm.ValueMember = "Realm";
-                
-                // Set Default Selected
-                string savedRealm = (string)Program._Config.GetSetting("Login_Realm");
-                if (!string.IsNullOrEmpty(savedRealm))
+                Program._Api = new ApiClient(server, port, validateSsl);
+                List<RealmData> realms = Program._Api.GetRealms();
+
+                if (realms != null && realms.Count > 0)
                 {
-                    comboBox_realm.SelectedValue = savedRealm;
-                } else
-                {
-                    comboBox_realm.SelectedValue = "pam";
+                    comboBox_realm.DataSource = realms;
+                    comboBox_realm.DisplayMember = "Comment";
+                    comboBox_realm.ValueMember = "Realm";
+                    comboBox_realm.SelectedValue = (string)Program._Config.GetSetting("Login_Realm") ?? "pam";
+                    panel_credentials.Visible = true;
+                    Width = 570;
                 }
-                
-                // Expand Client Login for next step & enable fields.
-                panel_credentials.Visible = true;
-                Width = 570;
+                else
+                {
+                    comboBox_realm.DataSource = null;
+                    Width = 290;
+                }
             }
-            else
-            {   
-                // If Server is Offline, Reset State
-                comboBox_realm.DataSource = null;
-                Width = 290;
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error connecting to server: {ex.Message}");
             }
-            
-            // Enable Fields & Center ClientLogin Window
-            CenterToScreen();
-            panel_credentials.Enabled = true;
-            panel_server.Enabled = true;
+            finally
+            {
+                CenterToScreen();
+                panel_credentials.Enabled = true;
+                panel_server.Enabled = true;
+            }
         }
 
         private void ValidateLogin(object sender, EventArgs e)
@@ -101,65 +80,68 @@ namespace Proxmox_Desktop_Client.Panels
             panel_server.Enabled = false;
             Program._Api = new ApiClient(server, port, validateSsl);
 
-            if (Program._Api.LoginRequest(username, password, realm, otp))
+            try
             {
-                SaveCredentials();
-                
-                if (Program._Panels.ContainsKey("MainPanel"))
+                if (Program._Api.LoginRequest(username, password, realm, otp))
                 {
-                    Program._Panels.Remove("MainPanel");
+                    SaveCredentials();
+                    if (Program._Panels.ContainsKey("MainPanel"))
+                    {
+                        Program._Panels.Remove("MainPanel");
+                    }
+                    MainPanel newWindow = new MainPanel();
+                    Program._Panels.Add("MainPanel", newWindow);
+                    Hide();
                 }
-                
-                MainPanel newWindow = new MainPanel();
-                Program._Panels.Add("MainPanel", newWindow);
-                Hide();
             }
-
-            panel_credentials.Enabled = true;
-            panel_server.Enabled = true;
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Login failed: {ex.Message}");
+            }
+            finally
+            {
+                panel_credentials.Enabled = true;
+                panel_server.Enabled = true;
+            }
         }
 
         private void SaveCredentials()
         {
             bool remember = checkBox_remember.Checked;
-
-            Program._Config.SetSetting("Login_Server", textBox_server.Text);
-            Program._Config.SetSetting("Login_Port", textBox_port.Text);
-            Program._Config.SetSetting("Login_CheckSSL", checkBox_ssl.Checked);
-            
+            SetConfigSetting("Login_Server", textBox_server.Text);
+            SetConfigSetting("Login_Port", textBox_port.Text);
+            SetConfigSetting("Login_CheckSSL", checkBox_ssl.Checked);
             if (remember)
             {
-                Program._Config.SetSetting("Login_Username", textBox_username.Text);
-                Program._Config.SetSetting("Login_Password", textBox_password.Text);
-                Program._Config.SetSetting("Login_Realm", comboBox_realm.SelectedValue);
-                Program._Config.SetSetting("Login_OTP", checkBox_otp.Checked);
+                SetConfigSetting("Login_Username", textBox_username.Text);
+                SetConfigSetting("Login_Password", textBox_password.Text);
+                SetConfigSetting("Login_Realm", comboBox_realm.SelectedValue);
+                SetConfigSetting("Login_OTP", checkBox_otp.Checked);
             }
             else
             {
-                Program._Config.SetSetting("Login_Username", "");
-                Program._Config.SetSetting("Login_Password", "");
-                Program._Config.SetSetting("Login_Realm", "pam");
-                Program._Config.SetSetting("Login_OTP", false);
+                SetConfigSetting("Login_Username", "");
+                SetConfigSetting("Login_Password", "");
+                SetConfigSetting("Login_Realm", "pam");
+                SetConfigSetting("Login_OTP", false);
             }
+            SetConfigSetting("Login_Remember", remember);
+        }
 
-            Program._Config.SetSetting("Login_Remember", remember);
+        private void SetConfigSetting(string key, object value)
+        {
+            Program._Config.SetSetting(key, value);
         }
 
         public void LoadCredentials()
         {
-            bool remember = (bool?) Program._Config.GetSetting("Login_Remember") ?? false;
-
+            bool remember = (bool?)Program._Config.GetSetting("Login_Remember") ?? false;
             if (remember)
             {
                 textBox_server.Text = (string)Program._Config.GetSetting("Login_Server");
                 textBox_port.Text = (string)Program._Config.GetSetting("Login_Port");
-                checkBox_ssl.Checked = (bool)Program._Config.GetSetting("Login_CheckSSL");    
-                
+                checkBox_ssl.Checked = (bool)Program._Config.GetSetting("Login_CheckSSL");
                 ClickCheckServer();
-            }
-
-            if (remember)
-            {
                 textBox_username.Text = (string)Program._Config.GetSetting("Login_Username");
                 textBox_password.Text = (string)Program._Config.GetSetting("Login_Password");
                 checkBox_otp.Checked = (bool)Program._Config.GetSetting("Login_OTP");
@@ -170,7 +152,6 @@ namespace Proxmox_Desktop_Client.Panels
                 textBox_password.Text = "";
                 checkBox_otp.Checked = false;
             }
-
             checkBox_remember.Checked = remember;
             textBox_otp.Visible = checkBox_otp.Checked;
         }
@@ -182,6 +163,5 @@ namespace Proxmox_Desktop_Client.Panels
                 e.Handled = true;
             }
         }
-
     }
 }
