70bc9c9469dcf0893adf2c19e430f61be49eed84
Matthew <matthew.bate@sk-projects.net>
2024-12-19 21:59:50 -0500

    Adding WebGUI Window

---
+++ Classes/consoles/WebGUI.Designer.cs
@@ -0,0 +1,49 @@
+﻿using System.ComponentModel;
+
+namespace Proxmox_Desktop_Client.Classes.consoles;
+
+partial class WebGUI
+{
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+        System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WebGUI));
+        this.SuspendLayout();
+        // 
+        // WebGUI
+        // 
+        this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        this.ClientSize = new System.Drawing.Size(800, 450);
+        this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+        this.Name = "WebGUI";
+        this.Text = "Proxmox VE Cluster GUI";
+        this.ResumeLayout(false);
+    }
+
+    #endregion
+}
---
+++ Classes/consoles/WebGUI.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2.WinForms;
+using Newtonsoft.Json;
+
+namespace Proxmox_Desktop_Client.Classes.consoles
+{
+    public partial class WebGUI : Form
+    {
+        private WebView2 webView;
+
+        public WebGUI()
+        {
+            InitializeComponent();
+            CenterToScreen();
+            InitializeWebView();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private async void InitializeWebView()
+        {
+            try
+            {
+                webView = new WebView2
+                {
+                    Dock = DockStyle.Fill
+                };
+                this.Controls.Add(webView);
+
+                string userDataFolder = Path.Combine(Program._Config.AppDataFolder, "WebView2Data");
+                var env = await CoreWebView2Environment.CreateAsync(null, userDataFolder, new CoreWebView2EnvironmentOptions("--ignore-certificate-errors"));
+                await webView.EnsureCoreWebView2Async(env);
+
+                if (webView.CoreWebView2 == null)
+                {
+                    Program.DebugPoint("WebView2 initialization failed.");
+                    return;
+                }
+
+                // Enable JavaScript
+                webView.CoreWebView2.Settings.IsScriptEnabled = true;
+
+                // Handle navigation events
+                webView.CoreWebView2.NavigationCompleted += WebView_NavigationCompleted;
+
+                string ticketValue = Program._Api.DataTicket.ticket;
+
+                var cookie = webView.CoreWebView2.CookieManager.CreateCookie(
+                    "PVEAuthCookie",
+                    ticketValue,
+                    Program._Api.DataServerInfo.server,
+                    "/"
+                );
+
+                cookie.IsHttpOnly = false;
+                cookie.SameSite = CoreWebView2CookieSameSiteKind.Lax;
+
+                webView.CoreWebView2.CookieManager.AddOrUpdateCookie(cookie);
+                
+                string WebURL = $"https://{Program._Api.DataServerInfo.server}:{Program._Api.DataServerInfo.port}/#v1:0:18:4:::::::";
+
+                webView.CoreWebView2.Navigate(WebURL);
+                
+            }
+            catch (Exception ex)
+            {
+                Program.DebugPoint(JsonConvert.SerializeObject(ex));
+            }
+        }
+
+        private void WebView_NavigationCompleted(object sender, CoreWebView2NavigationCompletedEventArgs e)
+        {
+            if (e.IsSuccess)
+            {
+                Program.DebugPoint("Navigation completed successfully.");
+            }
+            else
+            {
+                Program.DebugPoint($"Navigation failed with error: {e.WebErrorStatus}");
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            webView.Dispose();
+            Dispose();
+        }
+    }
+}
